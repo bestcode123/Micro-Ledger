55ac289d473a7df91061119c9170dd60101919bc
bestcode123 <burgerboy99@icloud.com>
2022-01-18 16:13:49 +0000

    v1.0.2
    Extra ln

--- l.py
+++ l.py
@@ -1,3 +1,4 @@
 import os
-for i in range(128):
-    os.system('mkdir ~/Documents/GitHub/Micro-Ledger/mining/defensive/ledger/' + str(i))
+for i2 in range(5):
+    for i in range(128):
+        os.system('mkdir -p ~/Documents/GitHub/Micro-Ledger/mining/defensive/ledger/' + str(i2) + "/" + str(i))
